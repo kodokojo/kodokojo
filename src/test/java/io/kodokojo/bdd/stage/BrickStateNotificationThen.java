7c437dc6ec7f98e3d031ad91e1dbb70cb5aa7e51
jpthiery <jpthiery@xebia.fr>
2016-04-26 18:49:27 +0200

    Attach all WebSocket notification in Int Tests

--- src/test/java/io/kodokojo/bdd/stage/BrickStateNotificationThen.java
+++ src/test/java/io/kodokojo/bdd/stage/BrickStateNotificationThen.java
@@ -2,17 +2,17 @@ package io.kodokojo.bdd.stage;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.tngtech.jgiven.CurrentStep;
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.attachment.Attachment;
 import io.kodokojo.entrypoint.dto.WebSocketMessage;
 import io.kodokojo.entrypoint.dto.WebSocketMessageGsonAdapter;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -33,6 +33,9 @@ public class BrickStateNotificationThen<SELF extends BrickStateNotificationThen<
     @ExpectedScenarioState
     CountDownLatch nbMessageExpected;
 
+    @ExpectedScenarioState
+    CurrentStep currentStep;
+
     public SELF i_receive_all_notification() {
         try {
             nbMessageExpected.await(2, TimeUnit.MINUTES);
@@ -42,7 +45,8 @@ public class BrickStateNotificationThen<SELF extends BrickStateNotificationThen<
         GsonBuilder builder = new GsonBuilder();
         builder.registerTypeAdapter(WebSocketMessage.class, new WebSocketMessageGsonAdapter());
         Gson gson = builder.create();
-        List<WebSocketMessage> webSocketMessages = listener.getMessages().stream().map(m -> gson.fromJson(m, WebSocketMessage.class)).collect(Collectors.toList());
+        LinkedList<String> srcMessages = new LinkedList<>(listener.getMessages());
+        List<WebSocketMessage> webSocketMessages = srcMessages.stream().map(m -> gson.fromJson(m, WebSocketMessage.class)).collect(Collectors.toList());
         Map<String, List<WebSocketMessage>> messagePerBrick = new HashMap<>();
         for (WebSocketMessage message : webSocketMessages) {
             if ("brick".equals(message.getEntity())) {
@@ -88,6 +92,8 @@ public class BrickStateNotificationThen<SELF extends BrickStateNotificationThen<
             assertThat(starting).isTrue();
             assertThat(running).isTrue();
         }
+
+        currentStep.addAttachment(Attachment.plainText(StringUtils.join(srcMessages, "\n")).withTitle("WebSocket messages receive").withFileName("websocketMessages"));
 
         return self();
     }
