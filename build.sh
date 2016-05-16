318558820ff40739ad23381b6c933225b47bf24c
jpthiery <jpthiery@xebia.fr>
2016-05-16 17:19:35 +0200

    Update Licence header

--- build.sh
+++ build.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+#
+# Kodo Kojo - Software factory done right
+# Copyright © 2016 Kodo Kojo (infos@kodokojo.io)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
 
 DOCKER_BIN_PATH=$(type -a docker | head -n1 | awk '{print $3}')
 if [ ! -x "$DOCKER_BIN_PATH" ]; then
--- src/main/java/io/kodokojo/model/Stack.java
+++ src/main/java/io/kodokojo/model/Stack.java
@@ -1,26 +1,22 @@
-package io.kodokojo.model;
-
-/*
- * #%L
- * kodokojo-commons
- * %%
- * Copyright (C) 2016 Kodo-kojo
- * %%
+/**
+ * Kodo Kojo - Software factory done right
+ * Copyright © 2016 Kodo Kojo (infos@kodokojo.io)
+ *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/gpl-3.0.html>.
- * #L%
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+package io.kodokojo.model;
+
 
 import java.io.Serializable;
 import java.util.Set;
