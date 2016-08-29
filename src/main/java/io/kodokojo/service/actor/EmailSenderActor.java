45c0e2d3a65d524d34b9cbfbf85b595f2b95a2fb
jpthiery <jpthiery@xebia.fr>
2016-08-29 17:38:48 +0200

    bad package import in EmailSenderActor

--- src/main/java/io/kodokojo/service/actor/EmailSenderActor.java
+++ src/main/java/io/kodokojo/service/actor/EmailSenderActor.java
@@ -1,17 +1,17 @@
 /**
  * Kodo Kojo - Software factory done right
  * Copyright © 2016 Kodo Kojo (infos@kodokojo.io)
- *
+ * <p>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -22,7 +22,6 @@ import akka.actor.Props;
 import akka.dispatch.Futures;
 import akka.event.LoggingAdapter;
 import akka.japi.pf.ReceiveBuilder;
-import hudson.model.Run;
 import io.kodokojo.service.EmailSender;
 
 import java.util.Arrays;
@@ -86,7 +85,7 @@ public class EmailSenderActor extends AbstractActor {
         }
 
         public EmailSenderMsg(List<String> to, String object, String content) {
-            this(to, null,null, object, content, true);
+            this(to, null, null, object, content, true);
         }
     }
 
