67bfacd951390d613089e13add361aab57e5e5ba
jpthiery <jpthiery@xebia.fr>
2016-09-09 12:01:41 +0200

    Adding licence header and scm in pom.xml

--- src/main/java/io/kodokojo/endpoint/ProjectSparkEndpoint.java
+++ src/main/java/io/kodokojo/endpoint/ProjectSparkEndpoint.java
@@ -1,17 +1,17 @@
 /**
  * Kodo Kojo - Software factory done right
  * Copyright © 2016 Kodo Kojo (infos@kodokojo.io)
- * <p>
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * <p>
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * <p>
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -146,6 +146,7 @@ public class ProjectSparkEndpoint extends AbstractSparkEndpoint {
                 halt(404);
                 return "";
             }
+            //TODO Call an Actor
             if (userIsAdmin(requester, projectConfiguration)) {
                 JsonParser parser = new JsonParser();
                 JsonArray root = (JsonArray) parser.parse(request.body());
--- src/main/java/io/kodokojo/service/actor/EmailSenderActor.java
+++ src/main/java/io/kodokojo/service/actor/EmailSenderActor.java
@@ -1,17 +1,17 @@
 /**
  * Kodo Kojo - Software factory done right
  * Copyright © 2016 Kodo Kojo (infos@kodokojo.io)
- * <p>
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * <p>
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * <p>
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -40,13 +40,10 @@ public class EmailSenderActor extends AbstractActor {
         return Props.create(EmailSenderActor.class, emailSender);
     }
 
-    private final EmailSender emailSender;
-
     public EmailSenderActor(EmailSender emailSender) {
         if (emailSender == null) {
             throw new IllegalArgumentException("emailSender must be defined.");
         }
-        this.emailSender = emailSender;
         receive(ReceiveBuilder.match(EmailSenderMsg.class, msg -> {
             try {
                 emailSender.send(msg.to, msg.cc, msg.ci, msg.object, msg.content, msg.htmlContent);
