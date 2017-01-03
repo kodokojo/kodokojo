0cb5f1c646d3107c6853563dcb71031cab37c27b
jpthiery <jpthiery@xebia.fr>
2017-01-03 11:52:09 +0100

    Update header licence

--- src/main/java/io/kodokojo/commons/model/DockerImageNameParser.java
+++ src/main/java/io/kodokojo/commons/model/DockerImageNameParser.java
@@ -1,3 +1,20 @@
+/**
+ * Kodo Kojo - Software factory done right
+ * Copyright © 2017 Kodo Kojo (infos@kodokojo.io)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 // Generated from io/kodokojo/commons/utils/DockerImageName.g4 by ANTLR 4.3
 package io.kodokojo.commons.model;
 import org.antlr.v4.runtime.atn.*;
